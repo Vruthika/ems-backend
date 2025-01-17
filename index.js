37d7a5995a00278db5694ca61e0c87a3d77c3427
Vruthika L S <126603138+Vruthika@users.noreply.github.com>
2025-01-17 23:11:08 +0530

    path

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-import express from 'express';
-import cors from 'cors';
-import authRouter from './routes/auth.js';
-import deptRouter from './routes/department.js';
-import employeeRouter from './routes/employee.js';
-import salaryRouter from './routes/salary.js'
-import leaveRouter from './routes/leave.js'
-import settingRouter from './routes/setting.js'
-import dashboardRouter from './routes/dashboard.js'
-import connectToDatabase from './db/db.js';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import authRouter from "./routes/auth.js";
+import deptRouter from "./routes/department.js";
+import employeeRouter from "./routes/employee.js";
+import salaryRouter from "./routes/salary.js";
+import leaveRouter from "./routes/leave.js";
+import settingRouter from "./routes/setting.js";
+import dashboardRouter from "./routes/dashboard.js";
+import connectToDatabase from "./db/db.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 connectToDatabase();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://ems-frontend-final.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(express.static('public/uploads'));
-app.use('/api/auth', authRouter);
-app.use('/api/department', deptRouter);
-app.use('/api/employee', employeeRouter);
-app.use('/api/salary', salaryRouter);
-app.use('/api/leave', leaveRouter);
-app.use('/api/setting', settingRouter);
-app.use('/api/dashboard', dashboardRouter);
+app.use(express.static("public/uploads"));
+app.use("/api/auth", authRouter);
+app.use("/api/department", deptRouter);
+app.use("/api/employee", employeeRouter);
+app.use("/api/salary", salaryRouter);
+app.use("/api/leave", leaveRouter);
+app.use("/api/setting", settingRouter);
+app.use("/api/dashboard", dashboardRouter);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server runs on the port ${process.env.PORT}`);
+  console.log(`Server runs on the port ${process.env.PORT}`);
 });
